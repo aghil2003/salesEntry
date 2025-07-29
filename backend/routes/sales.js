f281a4c1959075d1e6387d9e2e7c9fa8e60ea7a4
aghil <aghilpuduruthy680623@gmail.com>
2025-07-29 11:12:43 +0530

    final commit

--- backend/routes/sales.js
+++ backend/routes/sales.js
@@ -1,11 +1,3 @@
-// import express from 'express';
-// const router = express.Router();
-// import  salesController  from '../controllers/salesController.js';
-
-// router.post('/header/multiple', salesController.insertSalesEntry);
-
-// export const salesRoutes = router;
-
 
 import express from 'express';
 const router = express.Router();
